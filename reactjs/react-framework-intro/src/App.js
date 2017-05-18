f6d612d91be135ccd7208dc868f0fd31aee133ac
Wesley Ren <wren@walmartlabs.com>
2017-05-18 15:35:34 -0700

    added

---
+++ reactjs/react-framework-intro/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import './App.css';
+import User from './components/User'
+import Account from './components/Account'
+import Blog from './components/Blog'
+
+class App extends Component {
+  render() {
+    return (
+        <Router>
+          <div>
+            <div>
+              <Link to="/">Home</Link> |
+              <Link to="/account">Account</Link> |
+              <Link to="/blog">Blog</Link>
+            </div>
+            <hr/>
+            <Route exact path="/" component={User}></Route>
+            <Route path="/account" component={Account}></Route>
+            <Route path="/blog" component={Blog}></Route>
+          </div>
+        </Router>
+    );
+  }
+}
+
+export default App;
---
+++ reactjs/react-framework-intro/src/components/Info.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Info extends Component {
+    render() {
+        return (
+            <div className="Info">
+                <h1>The Info</h1>
+                <p>React JS is awesoem!</p>
+            </div>
+        );
+    }
+}
+
+export default Info;
